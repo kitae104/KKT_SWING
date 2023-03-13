62d4e568a0fdb9e0a660dfd07337cdf3a24dd254
Kitae's Github <45898428+kitae104@users.noreply.github.com>
2023-03-13 10:58:18 +0900

    자료 수정

--- src/Temp.java
+++
@@ -1,6 +0,0 @@
-
-public class Temp {
-	public static void main(String[] args) {
-		System.out.println("자바 응용 프로그램");
-	}
-}
--- src/Test.java
+++
@@ -1,9 +0,0 @@
-
-public class Test {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("자바 응용 프로그램");
-	}
-
-}
